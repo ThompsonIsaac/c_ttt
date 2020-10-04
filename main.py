837d714563f8b317654226c4c5c7e6ffd3a17d21
isaact23 <isaact23@vt.edu>
2020-10-04 19:25:40 -0400

    Simplify code

--- main.py
+++ main.py
@@ -18,82 +18,71 @@ class Node():
         self.score = 0 # 1 if X wins, -1 if O wins
         self.children = []
 
-    # Assign a score for this node
-    def score_self(self, board):
-        # Update board according to this node
-        self.update_board(board)
-        
-        # Generate score
-        winner = board.get_winner()
-        if winner == X:
-            self.score = 1
-        elif winner == O:
-            self.score = -1
+    # Get best move. Assumes minimax was already called
+    def get_best_move(self):
+        best_child = None
+        if self.player == X:
+            best_score = 100
+            for child in self.children:
+                if child.score < best_score:
+                    best_child = child
+                    best_score = child.score
         else:
-            self.score = 0
-        
-        # Revert board to before this node
-        self.revert_board(board)
-
-        return self.score
+            best_score = -100
+            for child in self.children:
+                if child.score > best_score:
+                    best_child = child
+                    best_score = child.score
+        return best_child.square
 
     # Add a level of nodes below this node
-    def add_level(self, board, alpha=-1, beta=1):
+    def minimax(self, board, depth=0, alpha=-100, beta=100):
+
         # Update board according to this parent node
         self.update_board(board)
 
         # Set our score to the best possible case - minimax will override later
         if self.player == X:
-            self.score = 1
+            self.score = 100
             next_player = O
         else:
-            self.score = -1
+            self.score = -100
             next_player = X
-        
-        # Create new children
-        if len(self.children) == 0 and board.get_winner() == None:
+
+        # If terminal node, just score this node (the higher the depth, the better)
+        if board.is_terminal() or depth == 0:
+            self.score = board.get_score()
+
+        # Otherwise create new children and evaluate
+        else:
             for i in range(9):
                 if board.is_square_open(i):
+                    
+                    # Create new node and branches
                     new_node = Node(i, next_player, False)
                     self.children.append(new_node)
-                    score = new_node.score_self(board)
-                    
-                    self.update_score(score)
+                    score = new_node.minimax(board, depth - 1, alpha, beta)
+
+                    # Update this node's score according to child score
+                    if self.player == X and score < self.score:
+                        self.score = score
+                    elif self.player == O and score > self.score:
+                        self.score = score
+
+                    # Apply alpha-beta pruning
                     if self.player == O:
                         alpha = max(alpha, score)
                         if (alpha >= beta):
-                            break
+                            pass
                     else:
                         beta = min(beta, score)
                         if (beta <= alpha):
-                            break
-                    
-        # Tell children to add level of nodes below them
-        else:
-            for child in self.children:
-                score = child.add_level(board, alpha, beta)
-
-                self.update_score(score)
-                if self.player == O:
-                    alpha = max(alpha, score)
-                    if (alpha >= beta):
-                        break
-                else:
-                    beta = min(beta, score)
-                    if (beta <= alpha):
-                        break
+                            pass
 
         # Revert board to before this parent node
         self.revert_board(board)
 
         return self.score
-
-    # Update current score according to minimax
-    def update_score(self, score):
-        if self.player == X and score < self.score:
-            self.score = score
-        elif self.player == O and score > self.score:
-            self.score = score
 
     # Update the board according to this node's move data
     def update_board(self, board):
@@ -105,7 +94,7 @@ class Node():
         if self.square != -1:
             board.undo_move(self.square)
 
-    # Print a traversal of the entire tree
+    # Print a traversal of the entire game tree
     def traverse(self, level=0):
         if self.is_root:
             print("Root Node - Score " + str(self.score))
@@ -121,27 +110,36 @@ class Board():
     def __init__(self):
         self.board = [EMPTY] * 9 # 3x3 grid
         self.winner = None
+        self.squares_left = 9 # Number of empty squares
 
+    # Print the board in a user-friendly way
     def print(self):
+        print()
+        print("    A   B   C\n")
         for i in range(3):
-            print(self.board[i*3] + " | " + self.board[(i*3)+1] + " | " + self.board[(i*3)+2])
+            print(str(i + 1) + " " * 3 + self.board[i*3] + " | " + self.board[(i*3)+1] + " | " + self.board[(i*3)+2])
             if i < 2:
-                print("-"*9)
+                print(" "*3 + "-"*9)
+        print()
 
     # Add move data to the board
     def move(self, player, square):
         assert self.is_square_open(square), "Cannot move on occupied tile"
         self.board[square] = player
+        self.squares_left -= 1
         self.update_winner()
 
     # Remove move data from the board
     def undo_move(self, square):
         assert not self.is_square_open(square), "Cannot undo a move that hasn't been taken"
         self.board[square] = EMPTY
+        self.squares_left += 1
         self.update_winner() # NOTE THAT WE COULD PROBABLY JUST SET THE WINNER TO NONE BECAUSE OF MINIMAX.
 
     # See if a square is available
     def is_square_open(self, square):
+        if square == -1:
+            return False
         return self.board[square] == EMPTY
 
     # Determine the current winner
@@ -169,25 +167,66 @@ class Board():
         return self.winner
 
     # Return true if the board is full
-    def is_board_full(self):
+    def is_full(self):
         for i in range(9):
             if self.board[i] == EMPTY:
                 return False
         return True
 
+    # Return true if the board is terminal
+    def is_terminal(self):
+        return self.is_full() or self.get_winner() != None
+
+    # Get the score of this board pos. -1 if O wins, 1 if X wins.
+    def get_score(self):
+        winner = self.get_winner()
+        if winner == X:
+            return self.squares_left
+        elif winner == O:
+            return -self.squares_left
+        return 0
+
+    # Convert a user-input coordinate (A3, 2b, etc.) to a number, or -1 if failure
+    def get_square_from_string(self, coord):
+        if not isinstance(coord, str):
+            return -1
+        if len(coord) != 2:
+            return -1
+        coord = coord.lower()
+        alpha = None
+        numb = None
+        if coord[0].isalpha() and coord[1].isnumeric():
+            alpha = coord[0]
+            numb = int(coord[1]) - 1
+        elif coord[1].isnumeric() and coord[1].isalpha():
+            alpha = coord[1]
+            numb = int(coord[0] - 1)
+        else:
+            return -1
+        if not alpha in ['a', 'b', 'c']:
+            return -1
+        if numb < 0 or numb > 2:
+            return -1
+        return {'a': 0, 'b': 1, 'c': 2}[alpha] + (int(numb) * 3)
+
 def main():
-    start = perf_counter()
-    print("Started.")
     board = Board()
-    board.move(O, 0)
-    board.move(O, 8)
-    board.print()
-    root = Node()
-    for i in range(6):
-        root.add_level(board)
-    #root.traverse()
-    print("Done.")
-    print(perf_counter() - start)
+
+    while True:
+        board.print()
+        square = -1
+        while not board.is_square_open(square):
+            move = input("Input move: (a2, 3c, etc.) ")
+            square = board.get_square_from_string(move)
+        board.move(X, square)
+
+        root = Node(player=X)
+        root.minimax(board, depth=4)
+        best_move = root.get_best_move()
+        if best_move == None:
+            print("Error: Could not find a move.")
+            break
+        board.move(O, best_move)
 
 if __name__ == "__main__":
     main()
