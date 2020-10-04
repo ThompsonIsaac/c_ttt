6529dd8493b29c8c5f55974805b3c489a2b5a75e
shubhhhhh <72159956+shubhhhhh@users.noreply.github.com>
2020-10-04 08:34:17 +0530

    Update testing.c

--- testing.c
+++ testing.c
@@ -5,12 +5,15 @@
 #include <stdlib.h> // Memory Management, String Conversions, etc
 
 int main(int argc, char** argv) {
-    int a, b;
+    int a, b, c;
     printf("Enter a number: ");
     scanf("%d", &a);
     printf("Enter another number: ");
     scanf("%d", &b);
+    c=a+b;
+    printf("The addition is %d\n", c);
     printf("The product is %d\n", a*b);
     printf("Address of a is &x\n", &a);
     printf("Address of b is &x\n", &b);
+getch();
 }
